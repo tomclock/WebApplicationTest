c39a6fb5441eff1bc75c2a092cc9d39a5adbeae0
tom <wangjiun@live.com>
2016-07-12 14:03:38 +0900

    test

--- WebApplicationTest/Startup.cs
+++ WebApplicationTest/Startup.cs
@@ -4,6 +4,9 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(WebApplicationTest.Startup))]
 namespace WebApplicationTest
 {
+    /// <summary>
+    /// the end point of program.
+    /// </summary>
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
